cc7729d0717b3623ebe4ad0b8c0b01b0f66ef115
Roman <47948974+arom0808@users.noreply.github.com>
2021-10-08 03:15:18 +0300

    naming bugfix

---
+++ parseErrorHandler/index.d.ts
@@ -0,0 +1,3 @@
+import { ServerResponse } from "http";
+declare const _default: <Type>(data: string, res: ServerResponse, dataObj: Type, testObjFunvc: (dataObj: Type) => Type) => Type | undefined;
+export default _default;
