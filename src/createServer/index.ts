f6de5efd1a1418652977e5199dd960f59097ff4b
Roman <47948974+arom0808@users.noreply.github.com>
2021-10-10 00:46:32 +0300

    create server onserverexit

--- src/createServer/index.ts
+++ src/createServer/index.ts
@@ -3,9 +3,11 @@ import {ServerOptions} from "https";
 import serverMain from "../serverMain";
 import RoutesDict from "../routesDict";
 
-export default (options: ServerOptions, hostport: { host: string, port: number }, routes: RoutesDict, onServerRun?: () => void) => {
+export default (options: ServerOptions, hostport: { host: string, port: number }, routes: RoutesDict, onServerRun?: () => void, onServerExit?: () => void) => {
     const server = https.createServer(options, async (req, res) => {
         await serverMain(req, res, routes);
     });
     server.listen(hostport.port, hostport.host, onServerRun);
+    if(onServerExit)
+        process.on('exit', onServerExit);
 }
