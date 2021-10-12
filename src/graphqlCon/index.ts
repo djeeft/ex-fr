1ab6de92fe4bcd377a6ce8726558366df87a7f5a
Roman <47948974+arom0808@users.noreply.github.com>
2021-10-12 04:53:45 +0300

    add graphqlCon

---
+++ src/graphqlCon/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios"
+import https from "https";
+
+const agent = new https.Agent({
+    rejectUnauthorized: process.env.NODE_ENV === "production"
+});
+
+const pclClient = axios.create({
+    responseType: "json",
+    timeout: 10000,
+    headers: {
+        Accept: "application/json"
+    }, httpsAgent: agent
+});
+
+export default async (data: string) => {
+    if (typeof process.env.GRAPHQL_URL !== "string")
+        throw  {error: "Can't find env.GRAPHQL_URL", code: 1};
+    try {
+        const res = await pclClient.post(process.env.GRAPHQL_URL, data, {headers:{'Content-Type':'application/graphql'}});
+        return res.data;
+    } catch (e: any) {
+        if (e === 400)
+            throw  {error: "Bad input data", code: 2};
+        if (e.isAxiosError) {
+            throw  {error: "Problem with internet", code: 3, ax_er: e};
+        }
+        throw {error: "Unknown", code: 4, er_obj: e};
+    }
+}
